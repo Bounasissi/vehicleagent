149b6fcc6d0c0b34cfc411dc91ecfe454fc48550
Bounasissi <98055511+Bounasissi@users.noreply.github.com>
2023-07-08 01:57:38 -0400

    Create __init__.py
    it is blank bro

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# Initialization file
+#
+# This file is used to mark the directory as a Python package.
+#
+# You can define package-level imports or configurations here.
+#
+# For example, if you have a module named 'module.py' inside this package,
+# you can import it using:
+# from . import module
+#
+# Additionally, you can perform any necessary setup or initialization
+# tasks for your package.
+#
+# This file is left empty - no specific initialization is required.
